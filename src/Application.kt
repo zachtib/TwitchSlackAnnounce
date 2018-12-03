5fb1b14dbe5b3bf9fb1ae9ee6e5089a1029a5855
Zach Tibbitts <ztibbitts@hchb.com>
2018-12-03 10:50:52 -0500

    hello, world

---
+++ src/Application.kt
@@ -0,0 +1,46 @@
+package com.zachtib
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.features.*
+import org.slf4j.event.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.gson.*
+import io.ktor.client.*
+import io.ktor.client.engine.apache.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(CallLogging) {
+        level = Level.INFO
+        filter { call -> call.request.path().startsWith("/") }
+    }
+
+    install(DefaultHeaders) {
+        header("X-Engine", "Ktor") // will send this header with each response
+    }
+
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+    val client = HttpClient(Apache) {
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        get("/json/gson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
+
